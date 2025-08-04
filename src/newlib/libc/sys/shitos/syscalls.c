95a02e59fb1d75972703be949287511264a26b0a
f76goat <indigolucas69@gmail.com>
2025-08-04 00:56:31 -0400

    working_on_adding_newlib

--- src/newlib/libc/sys/shitos/syscalls.c
+++ src/newlib/libc/sys/shitos/syscalls.c
@@ -6,43 +6,109 @@
 #include <sys/time.h>
 #include <stdio.h>
 
+#include "include/io.h"
+
 char *__env[1] = { 0 };
 char **environ = __env;
+
+#define COM1_PORT 0x3F8
 
 void _exit()
 {
   while (1) __asm__ volatile("cli; hlt");
 }
 
-int close(int file)
+int _close(int file)
 {
   return -1;
 }
 
-int execve(char *name, char **argv, char **env)
+int _execve(char *name, char **argv, char **env)
 {
   errno = ENOMEM;
   return -1;
 }
 
-int fork(void)
+int _fork(void)
 {
   errno = EAGAIN;
   return -1;
 }
 
-int fstat(int file, struct stat *st)
+int _fstat(int file, struct stat *st)
 {
   st->st_mode = S_IFCHR;
   return 0;
 }
 
-int getpid(void)
+int _getpid(void)
 {
   return 1;
 }
 
-int isatty(int file)
+int _isatty(int file)
 {
   return 1;
+}
+
+int _kill(int pid, int sig)
+{
+  errno = EINVAL;
+  return -1;
+}
+
+int _link(char *old, char *new)
+{
+  errno = EMLINK;
+  return -1;
+}
+
+int _open(const char *name, int flags, int mode)
+{
+  return -1;
+}
+
+int _read(int file, char *ptr, int len)
+{
+  return 0;
+}
+
+caddr_t _sbrk(int incr)
+{
+  extern char _end;
+  static char *heap_end;
+  char *prev_heap_end;
+
+  if (heap_end == 0) {
+    heap_end = &_end;
+  }
+
+  prev_heap_end = heap_end;
+  heap_end += incr;
+  return (caddr_t) prev_heap_end;
+}
+
+int _stat(char *file, struct stat *st)
+{
+  st->st_mode = S_IFCHR;
+  return -1;
+}
+
+clock_t _times(struct rms *buf)
+{
+  return -1;
+}
+
+int _unlink(char *name)
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int _write(int file, char *ptr, int len)
+{
+  for (int i = 0; i < len; ++i) {
+    outb(COM1_PORT, ptr[i]);
+  }
+  return len;
 }
