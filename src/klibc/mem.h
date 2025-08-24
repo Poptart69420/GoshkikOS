af50a261204bd9f6ce677005bd660e0e66604dbd
f76goat <indigolucas69@gmail.com>
2025-08-23 21:13:24 -0400

    build_scripts_and_crt_things_gonna_work_on_gdt_next_I_think

---
+++ src/klibc/mem.h
@@ -0,0 +1,11 @@
+#ifndef MEM_H_
+#define MEM_H_
+
+#include <stddef.h>
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+
+#endif // MEM_H_
